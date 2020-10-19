4c5fabf4b89c75ea5ff25caef600c1e4cd695f54
saurav <kumar00914@gmail.com>
2020-10-19 14:30:26 +0530

    Added Employee Daily Wage Computaion

--- EmpWageCalculation.sh
+++ EmpWageCalculation.sh
@@ -4,7 +4,11 @@ randomCheck=$((RANDOM%2));
 
 if [ $isPresent -eq $randomCheck ];
 then
-	echo "Employee is Present";
+   empRatePerHr=20;
+   empHrs=8;
+   salary=$(($empHrs*$empRatePerHr));
+   echo "Salary of the Employee : " $salary;
 else
-	echo "Employee is Absent";
+	salary=0;
+	echo "Salary of the Employee : " $salary;
 fi
