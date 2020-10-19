620c241b3d8291ad1c913c146dc2f0c90f542fad
saurav <kumar00914@gmail.com>
2020-10-19 15:06:18 +0530

    Employee salary calculated for a month

--- EmpWageCalculation.sh
+++ EmpWageCalculation.sh
@@ -2,15 +2,19 @@
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
+numWorkingDays=20;
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
 EmpCheck=$((RANDOM%3));
-
 case $EmpCheck in
 	$isFullTime)
 		empHrs=16;;
 	$isPartTime)
 		empHrs=8;;
 	*)
-		empHrs=0;;
+	empHrs=0;;
 esac
 salary=$(($empHrs*$empRatePerHr));
-echo "Salary of Employee is :"$salary;
+totalSalary=$(($totalSalary+$salary));
+done
+echo "Salary for a Month is : " $totalSalary;
